647bfc64aa087065b731e79ea3334943210d95c9
Giovanni Ciotta <giociotta@gmail.com>
2022-05-17 17:19:25 +0200

    starter

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// Visualizzare in pagina 5 numeri casuali.
+let myArr = [];
+let memoryN = document.getElementById ("numeri-mem");
+
+
+
+
+while (myArr.length < 5 ){
+    let randNum = Math.floor (Math.random() * 100) + 1;
+        
+        if (!myArr.includes(randNum )){
+            myArr.push (randNum);
+        }
+
+
+}
+memoryN.append (myArr);
+
+
+console.log(myArr);
+
+//  parte un timer di 30 secondi.
+
+
+
+// Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+
+
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+
