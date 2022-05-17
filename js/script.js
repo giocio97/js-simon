7b34bb83281f5978858a4967629c8c23af60980d
Giovanni Ciotta <giociotta@gmail.com>
2022-05-17 17:33:56 +0200

    pulizia

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+var seconds = 30;
 // Visualizzare in pagina 5 numeri casuali.
 let myArr = [];
 let memoryN = document.getElementById ("numeri-mem");
@@ -20,7 +21,9 @@ memoryN.append (myArr);
 console.log(myArr);
 
 //  parte un timer di 30 secondi.
-
+const cleaner = setInterval(() => {
+    memoryN.innerHTML = "";
+},seconds * 1000);
 
 
 // Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
