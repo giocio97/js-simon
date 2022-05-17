c1f93df6331d1200f38430f9c02a1cc324efb1da
Giovanni Ciotta <giociotta@gmail.com>
2022-05-17 18:56:25 +0200

    continuo

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
+let button = document.querySelector (`button`);
+let numeriUtente = document.getElementById ("numeri-utente");
+let numeriDigitati = document.querySelector("input");
+numeriDigitativalore = numeriDigitati.value;
+console.log(numeriDigitativalore);
+let buttonInvia = document.getElementById ("invia");
 var seconds = 30;
+
 // Visualizzare in pagina 5 numeri casuali.
 let myArr = [];
 let memoryN = document.getElementById ("numeri-mem");
 
+button.addEventListener("click",
 
-
-
-while (myArr.length < 5 ){
+() => {while (myArr.length < 5 ){
     let randNum = Math.floor (Math.random() * 100) + 1;
         
         if (!myArr.includes(randNum )){
@@ -20,15 +26,27 @@ memoryN.append (myArr);
 
 console.log(myArr);
 
+})
+
 //  parte un timer di 30 secondi.
 const cleaner = setInterval(() => {
     memoryN.innerHTML = "";
-},seconds * 1000);
+},1 * 1000);
+
 
 
 // Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+const userNum = setInterval(() => {
+    arrUser = [];
+    numeriUtente.classList.remove ("display-numeri");
+    arrUser.push(numeriDigitativalore );
+    console.log(arrUser);
+}, seconds *  1000);
+
+
 
 
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
+// confrontare i due array e inserire le condizioni per dire se sono presenti gli stessi numeri
 
